a69d132088d11814d8680a73c4b3b24b905089d1
しょうゆ <65059089+syoooooooooyu@users.noreply.github.com>
2020-11-01 17:36:31 +0900

    結構変えた。動くか分からんから試してみて
    Configを削除
    ->setQuitMessage();に追加しといた
    use (Player, Config)を削除
    $this->getLogger()->notice();からinfo();に
    ここは好みでいい

--- quitreason/src/apartkktrain/quitreason/Main.php
+++ quitreason/src/apartkktrain/quitreason/Main.php
@@ -3,59 +3,33 @@
 namespace apartkktrain\quitreason;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\Player;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\utils\Config;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
 class Main extends PluginBase implements Listener
 {
 
-  private $config;
-
     public function onEnable()
     {
-        $this->getLogger()->notice("----------------------");
-        $this->getLogger()->notice("quitplugin構築完了 BY APARTKKTRAIN。");
-        $this->getLogger()->notice("----------------------");
+        $this->getLogger()->info("----------------------");
+        $this->getLogger()->info("quitplugin構築完了 BY APARTKKTRAIN。");
+        $this->getLogger()->info("----------------------");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-
-                $this->config = new Config($this->getDataFolder() . "quitmessage.yml", Config::YAML, array(
-            "通常退室" => "§6[§b退室§6]§a %name が自らの意思で退室しました。",
-            "タイムアウト" => "§6[§b退室§6]§a $%ame がタイムアウトにより退室しました。",
-            "サーバーエラー" => "§6[§b退室§6]§a %name がサーバーエラーにより退室しました。\n§4サーバー管理者又は開発者まで報告をお願いします。",
-            ));
 
     }
 
     public function onquit(PlayerQuitEvent $event)
     {
     	$reason = $event->getQuitReason();
-    	$player = $event->getPlayer();
-    	$name   = $event->getPlayer()->getName();
-
-         ///取得
-        $quit1 = $this->config->get("通常退室") ;
-        $quit2 = $this->config->get("タイムアウト");
-        $quit3 = $this->config->get("サーバーエラー");
-        ///変数の置き換え
-        $quit1 = str_replace("%name", $name, $quit1);
-        $quit2 = str_replace("%name", $name, $quit2);
-        $quit3 = str_replace("%name", $name, $quit3);
+    	$name = $event->getPlayer()->getName();
     	if ($reason === 'client disconnect') {
-    		$event->setQuitMessage($quit1);
-    		return true;
-
-    	}
-    	if ($reason === 'timeout') {
-    		$event->setQuitMessage($quit2);
-    		return true;
-    	}
-    	if ($reason === 'Internal server error') {
-    		$event->setQuitMessage($quit3);
-    		return true;
+    		$event->setQuitMessage("§6[§b退室§6]§a{$name}が自らの意思で退室しました。");
+    	}elseif ($reason === 'timeout') {
+    		$event->setQuitMessage("§6[§b退室§6]§a {$name} がタイムアウトにより退室しました。");
+    	}elseif ($reason === 'Internal server error') {
+    		$event->setQuitMessage("§6[§b退室§6]§a {$name} がサーバーエラーにより退室しました。\n§4サーバー管理者又は開発者まで報告をお願いします。");	
     	}
     }
 }
